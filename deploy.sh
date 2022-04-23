d2811fe4567cd41820db021fbba15864be1f04cf
geekercai <cairunpeimail@gmail.com>
2022-04-23 19:22:39 +0800

    fix Github Action

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ npm run build # 生成静态文件
 cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
-if [ -z "$GITHUB_TOKEN" ]; then
+if [ -z "$ACCESS_TOKEN" ]; then
   msg='deploy'
   githubUrl=git@github.com:yuna2017/knowledgeBase.git
 else
   msg='来自Github Action的自动部署'
-  githubUrl=https://geekercai:${GITHUB_TOKEN}@github.com/yuna2017/knowledgeBase.git
+  githubUrl=https://geekercai:${ACCESS_TOKEN}@github.com/yuna2017/knowledgeBase.git
   git config --global user.name "geekercai"
   git config --global user.email "cairunpeimail@gmail.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl main:gh-pages # 推送到github
 
